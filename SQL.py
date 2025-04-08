48edb7844067f4aa60bf2a115b5f96d1f5a40dcf
Colter Radke <ceradke1@liberty.edu>
2025-04-08 11:34:18 -0400

    Merged competitors and winner ids

--- SQL.py
+++ SQL.py
@@ -15,36 +15,33 @@ DROP TABLE IF EXISTS Matches
 
 # Create Tables
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Person (
+CREATE TABLE Person (
     PersonID INTEGER PRIMARY KEY,
     FirstName TEXT NOT NULL,
     LastName TEXT NOT NULL,
     Wins INTEGER DEFAULT 0,
     Losses INTEGER DEFAULT 0,
-    ELO INTEGER DEFAULT 1000
+    ELO INTEGER DEFAULT 1000,
+    UNIQUE(FirstName, LastName)
 );
 ''')
 
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Matches (
+CREATE TABLE Matches (
     MatchID INTEGER PRIMARY KEY,
     Date TEXT NOT NULL,
-    Competitor1ID INTEGER NOT NULL,
-    Competitor2ID INTEGER NOT NULL,
-    Coach1ID INTEGER,
-    Coach2ID INTEGER,
-    Hypeman1ID INTEGER,
-    Hypeman2ID INTEGER,
     WinnerID INTEGER NOT NULL,
     LoserID INTEGER NOT NULL,
-    FOREIGN KEY (Competitor1ID) REFERENCES Person(PersonID),
-    FOREIGN KEY (Competitor2ID) REFERENCES Person(PersonID),
-    FOREIGN KEY (Coach1ID) REFERENCES Person(PersonID),
-    FOREIGN KEY (Coach2ID) REFERENCES Person(PersonID),
-    FOREIGN KEY (Hypeman1ID) REFERENCES Person(PersonID),
-    FOREIGN KEY (Hypeman2ID) REFERENCES Person(PersonID),
+    CoachWinnerID INTEGER,
+    CoachLoserID INTEGER,
+    HypemanWinnerID INTEGER,
+    HypemanLoserID INTEGER,
     FOREIGN KEY (WinnerID) REFERENCES Person(PersonID),
-    FOREIGN KEY (LoserID) REFERENCES Person(PersonID)
+    FOREIGN KEY (LoserID) REFERENCES Person(PersonID),
+    FOREIGN KEY (CoachWinnerID) REFERENCES Person(PersonID),
+    FOREIGN KEY (CoachLoserID) REFERENCES Person(PersonID),
+    FOREIGN KEY (HypemanWinnerID) REFERENCES Person(PersonID),
+    FOREIGN KEY (HypemanLoserID) REFERENCES Person(PersonID)
 );
 ''')
 
