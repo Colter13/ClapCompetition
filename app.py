44256c23a02024a451087869120e8b4c1d57b408
Colter Radke <ceradke1@liberty.edu>
2025-04-16 17:31:55 -0400

    changed port

--- app.py
+++ app.py
@@ -136,4 +136,4 @@ def list_matches():
     return render_template("matches.html", matches=matches)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=10000)
